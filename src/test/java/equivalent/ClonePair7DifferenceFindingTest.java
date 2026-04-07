b44679bfb23f97b70971ea70ca9c635268e4f169
Yoshiki Higo <yoshiki.higo@gmail.com>
2026-04-07 16:47:02 +0900

    test cases were added.

---
+++ src/test/java/equivalent/ClonePair7DifferenceFindingTest.java
@@ -0,0 +1,18 @@
+package equivalent;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ClonePair7DifferenceFindingTest {
+
+    private final ClonePair7 subject = new ClonePair7();
+
+    @Test
+    void methodsInterpretLeadingZeroStringsDifferently() {
+        String input = "010";
+
+        assertEquals(Long.valueOf(8L), subject.method1(input));
+        assertEquals(Long.valueOf(10L), subject.method2(input));
+    }
+}
